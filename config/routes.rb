1b861f1f4f1bf68301b0d0124d38ccebdfd09651
Christian Robinson <christian.robinson@amba.co>
2023-08-23 19:45:22 +0100

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
   root "home#index"
   get "/orders", to: "orders#index"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   get "/auth/callback", to: "shopify_auth#callback"
 end
